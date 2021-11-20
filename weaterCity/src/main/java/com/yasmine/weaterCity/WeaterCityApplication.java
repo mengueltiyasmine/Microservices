9c0b7d5b932120ed074c5589810144630da366f1
mengueltiyasmine <menguelti.yasmine@gmail.com>
2021-11-20 17:29:33 +0100

    Spring Initializr

---
+++ weaterCity/src/main/java/com/yasmine/weaterCity/WeaterCityApplication.java
@@ -0,0 +1,13 @@
+package com.yasmine.weaterCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaterCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaterCityApplication.class, args);
+	}
+
+}
---
+++ weaterCity/src/test/java/com/yasmine/weaterCity/WeaterCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasmine.weaterCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaterCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
