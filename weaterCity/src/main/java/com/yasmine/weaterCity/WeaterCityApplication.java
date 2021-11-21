e6cb8c1d0fe8e896f77615b3a4c2ea00d5295f08
mengueltiyasmine <menguelti.yasmine@gmail.com>
2021-11-21 22:30:54 +0100

    add Eureka server de register WeaterCity Server

--- weaterCity/src/main/java/com/yasmine/weaterCity/WeaterCityApplication.java
+++ weaterCity/src/main/java/com/yasmine/weaterCity/WeaterCityApplication.java
@@ -2,9 +2,10 @@ package com.yasmine.weaterCity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class WeaterCityApplication {
 
